70b4bc6b615d7af54ac290057c6539876b882eb9
zeuselproXD <91479958+zeuselproXD@users.noreply.github.com>
2025-12-03 09:53:30 -0500

    Refactor seydel11.m for new matrix and algorithm
    Updated matrix A and vector b for improved calculations. Modified the algorithm to ensure diagonal dominance and adjusted the iteration logic.

--- matlabfies/unidad3/seydel11.m
+++ matlabfies/unidad3/seydel11.m
@@ -1,14 +1,20 @@
-A=[3 1 1;4 6 1;3 4 9];
-b=[2 1 4]';x0=[0 0 0]';
-n=size(A);n=n(1);
+A=[3 2 2 2;4 7 4 5;3 4 8 3;2 5 3 7];
+b=[2 1 2 1]';
+%primero es diagonal dominante, si no lo es debe ser de norma menor a 1
+%if norm(A)>1
+%  a1=A(1,:);b1=b(1);
+%  A(1,:)=A(3,:);b(1)=b(3);
+%  A(3,:)=a1;b(3)=b1;
+%endif
+n=size(A);n=n(1);x0=zeros(n,1);
 for i=1:n
-  k1=A(i,i);
+  k1=A(i,i);A(i,i)=0;
   A(i,:)=A(i,:)/k1; b(i)=b(i)/k1;
 endfor
 v=x0;
 for i=1:40
   for j=1:n
-    v(j)=A(j,:)*v+b(j);
+    v(j)=b(j)-A(j,:)*v;
   endfor
   if norm(v-x0)<0.0001
     break
