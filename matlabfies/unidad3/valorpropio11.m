7179bd88f4a56056aa452c3e7d2f13e0395f7401
zeuselproXD <91479958+zeuselproXD@users.noreply.github.com>
2025-12-15 09:47:23 -0500

    Refactor valorpropio11 function for convergence

--- matlabfies/unidad3/valorpropio11.m
+++ matlabfies/unidad3/valorpropio11.m
@@ -1,14 +1,14 @@
 function yzz=valorpropio11
     x0=[1 0 1]';l0=1;
     A=[3 2 5; 2 5 4;5 4 3];
-    x0=A*x0;
-    for i=1:1
+    x1=A*x0;
+    for i=1:20
         l1=x1'*x1/(x1'*x0/l0);
-        x1=A*x0/l1;
+        x2=A*x1/l1;
         disp([i x1' l1])
         if abs(l1-l0)<0.0001
             break
         endif
-        x0=x1;l0=l1;
+        x0=x1;x1=x2;l0=l1;
     endfor
 endfunction
