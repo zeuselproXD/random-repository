4d754716d13c651222295907debdc5dd4d720aa3
zeuselproXD <91479958+zeuselproXD@users.noreply.github.com>
2025-12-15 11:10:15 -0500

    Fix matrix A and update eigenvalue logic
    Adjusted matrix A and modified eigenvalue calculation.

--- matlabfies/unidad3/valorpropioinvdesp11.m
+++ matlabfies/unidad3/valorpropioinvdesp11.m
@@ -1,12 +1,12 @@
 function yzz=valorpropioinvdesp11
     x0=[1 0 1]';l0=1;
-    A=[3 2 5; 2 5 4;5 4 3];A=A-0.5*eye(3);
+    A=[3 2 5; 2 5 4;5 4 3];A=A+0.5*eye(3);
     B=inv(A);
     x1=B*x0;
     for i=1:100
-        l1=x1'*x0*l0/(x1'*x1);
+        l1=x1'*x0*l0/(x1'*x1)-0.5;
         x2=B*x1*l1;
-        disp([i x2' l1])
+        disp([i l1])
         if abs(l1-l0)<0.001
             break
         endif
